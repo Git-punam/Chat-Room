f0001698f262a504431e3480ed4e19b20c9934d1
Git-punam <dutta.punam.2017@gmail.com>
2022-07-10 01:52:38 +0530

    [Feat]:Only upon successful authentication chat scren visibilty added

--- code.js
+++ code.js
@@ -1,8 +1,7 @@
-
 var database = firebase.database();
 
 // asking for username getting the message typed by the user
-let username = prompt("Please enter your name:","")
+let username = prompt("Please enter your name:","");
 let send = document.querySelector(".send-button")
 
 send.addEventListener('click', writeUserData)
@@ -10,9 +9,18 @@ send.addEventListener('click', writeUserData)
 // writing the message to database typed by user
 function writeUserData() {
   let msg = document.querySelector(".chat-textbox").value;
+  
+  let time = new Date();
+  let hrs = time.getHours();
+  let min = time.getMinutes();
+  if(hrs>=12 && hrs<23)
+      var dtime=`${hrs}:${min}`+"PM";
+  else
+      var dtime=`${hrs}:${min}`+"AM";
   firebase.database().ref('messages/').push().set({
     message : msg,
-    userName: username
+    userName: username,
+    textTime: dtime
 });
   document.querySelector(".chat-textbox").value = "";
 }
@@ -25,40 +33,107 @@ function writeUserData() {
 firebase.database().ref('messages').on('child_added', (snapshot) => {
   const data = snapshot.val();
 
-
   if(username == data.userName)
   {
+    
     var side = document.createElement("div");
     side.setAttribute("class", "right");
-    var displayMsg = document.createElement('p');
-    displayMsg.setAttribute("id", `m-${snapshot.key}`);
-    var child = document.createTextNode(`${data.userName}: ${data.message}`)
-    displayMsg.appendChild(child)
-    
 
-    // delete feature
-    var buttonDel = document.createElement("button");
-    buttonDel.setAttribute("onclick", "deleteMsg(this)");
-    buttonDel.setAttribute("data-id", snapshot.key);
-    buttonDel.setAttribute("id", `message-${snapshot.key}`);
-    buttonDel.setAttribute("class", "buttonDel");
-    side.appendChild(displayMsg)
-    side.appendChild(buttonDel)
-    document.querySelector(".main-section").appendChild(side)
-    document.getElementById(`message-${snapshot.key}`).innerHTML = "del";
+    //parent div for text-message and 'del' button
+    var divfortextDel = document.createElement("div");
+    divfortextDel.setAttribute("class","div-for-text-del")
+
+              //for displaying text message
+              var displayMsg = document.createElement('p');
+              displayMsg.setAttribute("class","text-message")
+              displayMsg.setAttribute("id", `m-${snapshot.key}`);
+              var child = document.createTextNode(`${data.userName}: ${data.message}`)
+              displayMsg.appendChild(child)
+              // delete feature
+              var buttonDel = document.createElement("button");
+              buttonDel.setAttribute("onclick", "deleteMsg(this)");
+              buttonDel.setAttribute("data-id", snapshot.key);
+              buttonDel.setAttribute("id", `message-${snapshot.key}`);
+              buttonDel.setAttribute("class", "buttonDel");
+
+              
+              divfortextDel.appendChild(displayMsg)
+              divfortextDel.appendChild(buttonDel)
+
+              //  document.getElementById(`message-${snapshot.key}`).innerHTML = "del";
+
+              
+
+    //parent div for image and time
+    var divforImageTime = document.createElement("div");
+    divforImageTime.setAttribute("class","parent-for-img-time")
+              //this is for the chat-head image
+              var divforImage = document.createElement("div");    
+              divforImage.setAttribute("class","div-for-image");
+              var chatImage = document.createElement("img");
+              chatImage.setAttribute("src","/image/chat-head.png");
+              divforImageTime.appendChild(chatImage);
+
+              //this is for the time displayed
+              var displayTime = document.createElement("div");     
+              displayTime.setAttribute("class","display-time");
+              var chatTime = document.createElement("p");
+              var chatTimeDisplay = document.createTextNode(`${data.textTime}`)
+              chatTime.appendChild(chatTimeDisplay)
+              displayTime.appendChild(chatTime);
+
+              divforImageTime.appendChild(divforImage)
+              divforImageTime.appendChild(displayTime)
+
+        side.appendChild(divfortextDel)
+        side.appendChild(divforImageTime)
+
+      document.querySelector(".main-section").appendChild(side)
+      document.getElementById(`message-${snapshot.key}`).innerHTML = "del";
+    
   }
   else
   {
+    // console.log(data.textTime);
     var side = document.createElement("div");
     side.setAttribute("class", "left");
-    var displayMsg = document.createElement('p');
-    var child = document.createTextNode(`${data.userName}: ${data.message}`)
-    displayMsg.appendChild(child)
-    side.appendChild(displayMsg)
-    document.querySelector(".main-section").appendChild(side)
+
+      //parent for chat head and time display
+      var divforImageTime = document.createElement("div");
+      divforImageTime.setAttribute("class","parent-for-img-time")
+
+        //this is for the chat-head image
+        var divforImage = document.createElement("div");    
+        divforImage.setAttribute("class","div-for-image");
+        var chatImage = document.createElement("img");
+        chatImage.setAttribute("src","/image/chat-head.png");
+        divforImageTime.appendChild(chatImage);
+
+        //this is for the time displayed
+        var displayTime = document.createElement("div");     
+        displayTime.setAttribute("class","display-time");
+        var chatTime = document.createElement("p");
+        var chatTimeDisplay = document.createTextNode(`${data.textTime}`)
+        chatTime.appendChild(chatTimeDisplay)
+        displayTime.appendChild(chatTime);
+
+        divforImageTime.appendChild(divforImage)
+        divforImageTime.appendChild(displayTime)
+
+      //for diplaying text message
+      var displayMsg = document.createElement('p');
+      displayMsg.setAttribute("class","text-message");
+      var child = document.createTextNode(`${data.userName}: ${data.message}`)
+      displayMsg.appendChild(child)
+
+      side.appendChild(divforImageTime)
+      side.appendChild(displayMsg)
+
+      document.querySelector(".main-section").appendChild(side)
   }
   
   // var msgscroll = document.querySelector(".main-section")
+  // console.log(msgscroll.scrollHeight);
   // autoscrool();
   // function autoscrool() {
   //   // var h=msgscroll.scrollHeight;
@@ -76,6 +151,7 @@ firebase.database().ref("messages/").on("child_removed", function (snapshot) {
 });
 
 function deleteMsg(self) {
+  console.log(self);
   var messageId = self.getAttribute("data-id");
   database.ref('messages/').child(messageId).remove()
 }
@@ -91,7 +167,6 @@ function passcode() {
 }
 
 
-
 //authentication
 function user_access() {
   var user_input = document.getElementById("user_input").value;
@@ -99,10 +174,11 @@ function user_access() {
   database.ref('userpasscode/').on('child_added', (snapshot) => {
     let userCode = snapshot.val();
 
-    console.log(userCode);
+    // console.log(userCode);
 
       if (user_input == userCode.password) {
         document.querySelector(".passcode").style.display = "none";
+        document.querySelector(".chat").classList.add('chat-screen');
       }
        else if(user_input == ""){
         alert("Please Enter A Passcode")
